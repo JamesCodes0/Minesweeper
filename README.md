d5179866f4d74fc94d77a880114eb02a2a01435e
James <jtsetsekas@gmail.com>
2020-09-09 16:03:10 -0400

    first commit
    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# [Minesweeper](https://jamestsetsekas.com/ms/)
+
+Windows XP Minesweepr in React + Hooks.  
+Also support mobile.📱🎉
+
+Features:
+
+- Release double mouse press at the same time to open ceils automatically
+- **Never** game over for the first click
+- Switch difficulty from the menu
+- Support mobile! (long press for placing flags)
+
+
+## Try it now!
+Minesweeper 👉 https://jamestsetsekas.com/ms/
+
+![](https://i.imgur.com/EoSzjCx.png)
+![](https://i.imgur.com/ryy5BiC.png)
+![](https://i.imgur.com/IdEUMu8.png)
+# minesweeper
---
+++ build/precache-manifest.1c3ffe61c91f3eda63368fdecf74c5dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bae6a193fba830dc803c",
+    "url": "/ms/static/css/main.79a34787.chunk.css"
+  },
+  {
+    "revision": "bae6a193fba830dc803c",
+    "url": "/ms/static/js/main.bae6a193.chunk.js"
+  },
+  {
+    "revision": "23bb943fab267ea3d677",
+    "url": "/ms/static/js/runtime~main.23bb943f.js"
+  },
+  {
+    "revision": "e2d141ee29fc7fe9a2bb",
+    "url": "/ms/static/js/2.e2d141ee.chunk.js"
+  },
+  {
+    "revision": "e0cd3f8c02735fd69b54fc364cbfa37e",
+    "url": "/ms/index.html"
+  }
+];
